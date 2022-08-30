9163600bb6afcb2d53516a73a3ad0220185a3a84
kavya4630 <111385390+kavya4630@users.noreply.github.com>
2022-08-30 01:13:37 -0500

    Initial commit

---
+++ 1.1a.py
@@ -0,0 +1,9 @@
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+#we use the sort function to sort the list
+ages.sort()
+#printing the sorted list
+print(ages)
+#printing the maximum age present in the list
+print("maximum age is : ",max(ages))
+#printing the minimum age present in the list
+print("minimum age is : ",min(ages))
